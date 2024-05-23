f4e4434c613815829e27a8ad282e8a81724d3eab
JOAO ROBERTO DE OLIVEIRA FELIX <ROBERTO6262@DIGITALCOLLEGE.LOCAL>
2024-05-23 20:54:46 -0300

    Subindo teste de JS

---
+++ js/script2.js
@@ -0,0 +1,20 @@
+/* 
+Objetivo: Calcular a média de 2 números
+
+1 - Adquirir os dois números
+2 - Somar os dois números
+3- Dividir a soma pela quantidade
+*/
+
+let n1 = 100;
+let n2 = 900;
+
+function Resultado() {
+    let soma = n1 + n2;
+    let media = soma / 2;
+
+    console.log(media);
+    
+}
+
+Resultado()
