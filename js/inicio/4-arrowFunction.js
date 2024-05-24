14007a055c12ab44ff2365d02acfb67cf2ad737e
JOAO ROBERTO DE OLIVEIRA FELIX <ROBERTO6262@DIGITALCOLLEGE.LOCAL>
2024-05-23 21:51:03 -0300

    Subindo algumas alterações - estudar

---
+++ js/inicio/4-arrowFunction.js
@@ -0,0 +1,11 @@
+let nome = "João Roberto";
+
+/*function converterMaiuscula() {
+    return nome.toUpperCase();
+}*/
+
+function converterMaiuscula(texto) {
+    return texto.toUpperCase();
+}
+
+console.log(converterMaiuscula("João Roberto"))
--- js/script2.js
+++ js/script2.js
@@ -6,7 +6,7 @@ Objetivo: Calcular a média de 2 números
 3- Dividir a soma pela quantidade
 */
 
-let n1 = 100;
+/*let n1 = 100;
 let n2 = 900;
 
 function Resultado() {
@@ -17,4 +17,42 @@ function Resultado() {
     
 }
 
-Resultado()
+Resultado()*/
+
+/* 
+    Objetivo: Calcular a média de 2 números
+
+    1 - Pegar os dois números
+    2 - Somar os dois números
+    3 - Dividir a soma pela quantidade
+*/
+
+/*let n1 = 100;
+let n2 = 900;*/
+
+
+
+/* function media() {
+    console.log((n1+n2) / 2)
+} */
+
+/*function media() {
+    console.log(soma() / 2)
+}
+
+media()*/
+
+/*
+Objetivo: Calcular a média de quaisquer 2 números
+*/
+
+function soma (n1,n2) {
+    return n1+n2
+}
+
+function media2numeros(n1, n2) {
+    let resultado = soma(n1, n2) / 2
+    return resultado
+}
+
+console.log(media2numeros(214965, 879521))
