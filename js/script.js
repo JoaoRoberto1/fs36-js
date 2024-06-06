c2db1276e7ec2e7b9c626252b777e3729e2d899e
JOAO ROBERTO DE OLIVEIRA FELIX <ROBERTO6262@DIGITALCOLLEGE.LOCAL>
2024-06-06 20:02:30 -0300

    Subindo modificação

--- js/script.js
+++ js/script.js
@@ -9,6 +9,57 @@ function soma() {
 
 
     console.log(resultado);
+
+    // exibindo o resultado da div rasultado atráves da prop innerHTMl
+    let conteudoDIV = document.getElementsByClassName('resultado')[0];
+
+    conteudoDIV.innerHTML = resultado 
 }
 
+function subtrair() {
+    let numero1 = parseFloat(document.getElementById('n1').value);
+    let numero2 = parseFloat(document.getElementById('n2').value);
 
+
+    let resultado = numero1 - numero2
+
+
+    console.log(resultado);
+
+    // exibindo o resultado da div rasultado atráves da prop innerHTMl
+    let conteudoDIV = document.getElementsByClassName('resultado')[0];
+
+    conteudoDIV.innerHTML = resultado 
+}
+
+function multiplicar() {
+    let numero1 = parseFloat(document.getElementById('n1').value);
+    let numero2 = parseFloat(document.getElementById('n2').value);
+
+
+    let resultado = numero1 * numero2
+
+
+    console.log(resultado);
+
+    // exibindo o resultado da div rasultado atráves da prop innerHTMl
+    let conteudoDIV = document.getElementsByClassName('resultado')[0];
+
+    conteudoDIV.innerHTML = resultado 
+}
+
+function dividir() {
+    let numero1 = parseFloat(document.getElementById('n1').value);
+    let numero2 = parseFloat(document.getElementById('n2').value);
+
+
+    let resultado = numero1 / numero2
+
+
+    console.log(resultado);
+
+    // exibindo o resultado da div rasultado atráves da prop innerHTMl
+    let conteudoDIV = document.getElementsByClassName('resultado')[0];
+
+    conteudoDIV.innerHTML = resultado 
+}
