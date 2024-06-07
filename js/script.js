b3e5641ce6c77e2b31b6625379e9dc89945234ad
JOAO ROBERTO DE OLIVEIRA FELIX <ROBERTO6262@DIGITALCOLLEGE.LOCAL>
2024-06-06 21:51:36 -0300

    Subindo

--- js/script.js
+++ js/script.js
@@ -1,65 +1,34 @@
 // console.log('Script via body', document.getElementById('n1'))
 
+const numero1 = (document.getElementById('n1'));
+const numero2 = (document.getElementById('n2'));
+const conteudoDiv = document.querySelector('.resultado')
+
 function soma() {
-    let numero1 = parseFloat(document.getElementById('n1').value);
-    let numero2 = parseFloat(document.getElementById('n2').value);
-
-
-    let resultado = numero1 + numero2
-
-
-    console.log(resultado);
-
-    // exibindo o resultado da div rasultado atráves da prop innerHTMl
-    let conteudoDIV = document.getElementsByClassName('resultado')[0];
-
-    conteudoDIV.innerHTML = resultado 
+    let resultado = parseFloat(numero1.value) + parseFloat(numero2.value)
+    exibirResultado(resultado)
 }
 
 function subtrair() {
-    let numero1 = parseFloat(document.getElementById('n1').value);
-    let numero2 = parseFloat(document.getElementById('n2').value);
-
-
-    let resultado = numero1 - numero2
-
-
-    console.log(resultado);
-
-    // exibindo o resultado da div rasultado atráves da prop innerHTMl
-    let conteudoDIV = document.getElementsByClassName('resultado')[0];
-
-    conteudoDIV.innerHTML = resultado 
+    let resultado = parseFloat(numero1.value) - parseFloat(numero2.value)
+    exibirResultado(resultado)
 }
 
 function multiplicar() {
-    let numero1 = parseFloat(document.getElementById('n1').value);
-    let numero2 = parseFloat(document.getElementById('n2').value);
-
-
-    let resultado = numero1 * numero2
-
-
-    console.log(resultado);
-
-    // exibindo o resultado da div rasultado atráves da prop innerHTMl
-    let conteudoDIV = document.getElementsByClassName('resultado')[0];
-
-    conteudoDIV.innerHTML = resultado 
+    let resultado = parseFloat(numero1.value) * parseFloat(numero2.value)
+    exibirResultado(resultado)
 }
 
 function dividir() {
-    let numero1 = parseFloat(document.getElementById('n1').value);
-    let numero2 = parseFloat(document.getElementById('n2').value);
+    let resultado = parseFloat(numero1.value) / parseFloat(numero2.value)
+    exibirResultado(resultado)
+}
 
+function exibirResultado(resultado) {
+    conteudoDiv.innerHTML = resultado
+}
 
-    let resultado = numero1 / numero2
-
-
-    console.log(resultado);
-
-    // exibindo o resultado da div rasultado atráves da prop innerHTMl
-    let conteudoDIV = document.getElementsByClassName('resultado')[0];
-
-    conteudoDIV.innerHTML = resultado 
+function limparresultado() {
+    conteudoDiv.computedStyleMap.display = 'flex';
+    conteudoDiv.innerHTML = 'Resultado: ' + result
 }
